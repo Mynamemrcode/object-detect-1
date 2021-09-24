15b4382690c43f5f0a6c7d2091fd8c868bc6e600
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-09-23 17:03:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function me() {
+    window.location = "me.html"
+}
+function bd() {
+    window.location = "bd.html"
+}
+function comp() {
+    window.location = "comp.html"
+}
+function plan() {
+    window.location = "plan.html"
+}
+function heli() {
+    window.location = "heli.html"
+}
